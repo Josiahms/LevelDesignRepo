f15a357b333a3cc83111e2f6fef3e3e1f929c2a6
josiahms <stewartjos@uwplatt.edu>
2019-07-22 22:26:42 -0500

    Moved files around

---
+++ Assets/FrostPunk/Scripts/System/Utilities/DontDestroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+   private static List<Object> objects = new List<Object>();
+
+   private void Awake() {
+      DontDestroyOnLoad(gameObject);
+      objects.Add(gameObject);
+   }
+
+   public static void DestroyAll() {
+      objects.ForEach(x => Destroy(x));
+      objects.Clear();
+   }
+}
